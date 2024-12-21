64edc06c05bf6c9d3e78d9d954feed3e0db4a625
sushantmalik230802 <sushantmalik2300@gmail.com>
2024-12-21 21:42:30 +0530

    third commit

--- Test1.java
+++ Test1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Test{
+public class Test1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the first no:");
